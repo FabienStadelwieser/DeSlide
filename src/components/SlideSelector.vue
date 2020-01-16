5d9a24d8c69b3f5a9ae2faced2bb3181e18f5ff3
Fabien Stadelwieser <fabien@42l.fr>
2020-01-16 10:09:19 +0100

    working slider

--- src/components/SlideSelector.vue
+++ src/components/SlideSelector.vue
@@ -1,12 +1,46 @@
 <template lang="html">
-	<div>
+	<div id="slide-selector">
+		<div class="clic-slide" v-for="slide in getSlides" :key="slide.nbr" >
+			<router-link :to='"/edit/" + slide.nbr'>
+				<Preview class="preview"/>
+			</router-link>
+		</div>
 	</div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+import Preview from "./Preview.vue";
+
 export default {
+	name: "SlideSelector",
+	components: {
+		Preview,
+	},
+	computed: mapGetters(["getSlides"]),
 }
 </script>
 
 <style lang="css" scoped>
+#slide-selector {
+	background-color: #232348;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding: 10%;
+	overflow-y: scroll;
+}
+
+.clic-slide {
+	background-color: #fff;
+	width: 100%;
+	margin-bottom: 10%;
+	height: 9em;
+}
+
+.preview {
+	height: 100%;
+	width: 100%;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Vuex from 'vuex'
 
 import App from './App.vue'
 
@@ -14,9 +15,9 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'jquery/dist/jquery.min'
 import 'bootstrap/dist/js/bootstrap.min'
 
-Vue.use(VueRouter);
-
 Vue.config.productionTip = false;
+
+Vue.use(VueRouter);
 
 const router = new VueRouter({
 	routes: [
@@ -30,7 +31,83 @@ const router = new VueRouter({
 	mode: 'history'
 })
 
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+	state: {
+		slide: [
+			{ // One slide
+				nbr: 0,
+				elem: [
+					{
+						type: "title",
+						x: 0,
+						y: 0,
+					}
+				],
+			},
+			{ // One slide
+				nbr: 1,
+				elem: [
+					{
+						type: "title",
+						x: 0,
+						y: 0,
+					}
+				],
+			},
+			{ // One slide
+				nbr: 2,
+				elem: [
+					{
+						type: "title",
+						x: 0,
+						y: 0,
+					}
+				],
+			},
+			{ // One slide
+				nbr: 3,
+				elem: [
+					{
+						type: "title",
+						x: 0,
+						y: 0,
+					}
+				],
+			},
+		],
+		currentSlide: 0
+	},
+	getters: {
+		getSlides(state) {
+			return state.slide;
+		},
+		getSelectedSlide(state) {
+			return state.slide[state.currentSlide];
+		},
+		getSpecificSlide(state, id) {
+			return state.slide[id];
+		},
+		getId(state) {
+			return state.currentSlide;
+		}
+	},
+	mutations: {
+		addSlide(state, newSlide) {
+			state.slide.push(newSlide);
+		},
+		changeSelected(state, newId) {
+			state.currentSlide = newId
+		},
+		modifCurrentSlide(state, slide) {
+			state.slide[state.currentSlide] = slide;
+		},
+	},
+});
+
 new Vue({
 	router,
+	store,
 	render: h => h(App),
 }).$mount('#app')
